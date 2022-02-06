a2b6df280127f21a5bebb0f6143e06ac636849ca
cindberg <christopher.lindberg@helsinki.fi>
2022-02-06 22:05:50 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from datetime import timedelta
 
 app = Flask(__name__)
 
+POSTGRES_URL='localhost'
+POSTGRES_USER='postgres'
+POSTGRES_PW='admin'
+POSTGRES_DB='postgres'
+DB_URL = 'postgresql+psycopg2://{user}:{password}@{url}/{db}'.format(user=POSTGRES_USER,
+                                                                     password=POSTGRES_PW,
+                                                                     url=POSTGRES_URL,
+                                                                     db=POSTGRES_DB)
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SECRET_KEY'] = 'admin1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # hiljentää muistutukset, (turha voi poistaa)
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=60) #(turha lisä voi poistaa)
+CORS(app)
 
-def koti():  # etusivu
-
-    return 'Työnseuranta'
-
-def ominaisuudet(): #featuret
-
-    return 'Ominaisuudet!'
+db = SQLAlchemy(app)
 
 
 
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    from nakyma import *
+    app.run(debug=True, port='5002')
