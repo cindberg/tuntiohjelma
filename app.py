3050483656bd0b80aa86f52eea757cf3f57a5caa
cindberg <christopher.lindberg@helsinki.fi>
2022-02-22 20:24:32 +0200

    maybe fix heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request, session, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -8,14 +9,16 @@ load_dotenv()
 
 app = Flask(__name__)
 
+
+
 POSTGRES_URL = 'localhost'
 POSTGRES_USER = 'postgres'
 POSTGRES_PW = 'admin'
 POSTGRES_DB = 'postgres'
 DB_URL = 'postgresql+psycopg2://{user}:{password}@{url}/{db}'.format(user=POSTGRES_USER,
-                                                                     password=POSTGRES_PW,
-                                                                     url=POSTGRES_URL,
-                                                                     db=POSTGRES_DB)
+                                                                                                  password=POSTGRES_PW,
+                                                                                                  url=POSTGRES_URL,
+                                                                                                  db=POSTGRES_DB)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SECRET_KEY'] = 'admin1'
@@ -27,5 +30,4 @@ db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     from views import *
-
     app.run(debug=True)
